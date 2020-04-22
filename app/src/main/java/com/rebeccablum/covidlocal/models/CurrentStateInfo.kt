6fb9bbf32cd7608f275a64ddc502bdcb27b07774
Rebecca Blum <schmecs@gmail.com>
2020-04-22 14:31:30 -0400

    Add basic stats to state display page

--- app/src/main/java/com/rebeccablum/covidlocal/models/CurrentStateInfo.kt
+++ app/src/main/java/com/rebeccablum/covidlocal/models/CurrentStateInfo.kt
@@ -1,6 +1,8 @@
 package com.rebeccablum.covidlocal.models
 
+import com.rebeccablum.covidlocal.toStringOrUnknown
 import com.squareup.moshi.Json
+import kotlin.math.round
 
 data class CurrentStateInfo(
     @field:Json(name = "state") val state: String,
@@ -17,5 +19,22 @@ data class CurrentStateInfo(
     @field:Json(name = "lastUpdateEt") val lastUpdateEt: String,
     @field:Json(name = "death") val deaths: Int?,
     @field:Json(name = "totalTestResults") val totalTestResults: Int?
-)
+) {
+    fun positiveString() = positive.toStringOrUnknown()
+    fun negativeString() = negative.toStringOrUnknown()
+    fun hospitalizedCurrentlyString() = hospitalizedCurrently.toStringOrUnknown()
+    fun hospitalizedCumulativeString() = hospitalizedCumulative.toStringOrUnknown()
+    fun deathsString() = deaths.toStringOrUnknown()
 
+    fun percentPositiveString() = if (positive != null && negative != null) {
+        ((positive.toFloat() / (positive.toFloat() + negative.toFloat())) * 100).toInt().toString() + "%"
+    } else {
+        "Unknown"
+    }
+
+    fun mortalityRateString() = if (positive != null && deaths != null) {
+        round(deaths.toFloat() / positive.toFloat() * 100).toString() + "%"
+    } else {
+        "Unknown"
+    }
+}
